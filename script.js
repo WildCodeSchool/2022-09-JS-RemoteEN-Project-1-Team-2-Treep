e66c507514b8c85cb266bae53bbe5297611473ff
Osvaldo Felices <osvfelices@yahoo.com>
2022-09-28 11:55:27 +0200

    Adding timeline

--- script.js
+++ script.js
@@ -1,40 +1,109 @@
-function openForm() {
+const line = document.querySelector(".timeline-innerline");
 
-    document.getElementById("popupForm").style.display = "block";
-    document.getElementById("treenum").value = "";
-    
-    thanksbutton = document.getElementById("thanksbutton");
-    thanksbutton.addEventListener("click", 
-    function thanks(){
-      const name = document.getElementById("name").value;
-      let treenum = document.getElementById("treenum").value;
+let i = 0;
+let i2 = 1;
+let target1 = document.querySelector(".timeline ul");
+let target2 = document.querySelectorAll(".timeline ul li");
 
-        switch(true){
-      
-      case (treenum == 1):
-        document.getElementById("message").innerHTML = `${name}, thank you for giving a tree!`;
-        document.getElementById("nametree").value = "";
-        break;
+const timeline_events = document.querySelectorAll("ul li");
 
-      case (treenum > 1):
-        document.getElementById("message").innerHTML = `${name}, thank you for giving ${treenum} trees!`;
-        document.getElementById("nametree").value = "";
-        break;
+function showTime(e) {
+  e.setAttribute("done", "true");
+  e.querySelector(".timeline-point").style.background = "black";
+  e.querySelector(".date").style.opacity = "100%";
+  e.querySelector("p").style.opacity = "100%";
+  e.querySelector("p").style.transform = "translateY(0px)";
+}
 
-      default:
-        document.getElementById("message").innerHTML = `Invalid Number of Trees, please try again`;
+function hideTime(e) {
+  e.removeAttribute("done");
+  e.querySelector(".timeline-point").style.background = "black";
+  e.querySelector(".date").style.opacity = "0%";
+  e.querySelector("p").style.opacity = "0%";
+  e.querySelector("p").style.transform = "translateY(-10px)";
+}
+
+function slowLoop() {
+  setTimeout(function () {
+    showTime(timeline_events[i]);
+    timelineProgress(i + 1);
+    i++;
+    if (i < timeline_events.length) {
+      slowLoop();
+    }
+  }, 800);
+}
+
+
+function timelineProgress(value) {
+  let progress = `${(value / timeline_events.length) * 100}%`;
+  if (window.matchMedia("(min-width: 728px)").matches) {
+    line.style.width = progress;
+    line.style.height = "4px";
+  } else {
+    line.style.height = progress;
+    line.style.width = "4px";
+  }
+}
+
+let observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.intersectionRatio > 0.9) {
+        if (window.matchMedia("(min-width: 728px)").matches) {
+          slowLoop();
+        } else {
+          showTime(entry.target);
+          timelineProgress(i2);
+          i2++;
+        }
+        observer.unobserve(entry.target);
       }
+    });
+  },
+  { threshold: 1, rootMargin: "0px 0px -50px 0px" }
+);
 
-    document.getElementById("popupForm2").style.display = "block";
-    document.getElementById("popupForm").style.display = "none";
-})};
+if (window.matchMedia("(min-width: 728px)").matches) {
+  observer.observe(target1);
+} else {
+  target2.forEach((t) => {
+    observer.observe(t);
+  });
+}
 
 
-function closeForm() {
-    document.getElementById("popupForm").style.display = "none";
-  };
+timeline_events.forEach((li, index) => {
+  li.addEventListener("click", () => {
+    if (li.getAttribute("done")) {
+      timelineProgress(index);
 
+      // hide all timeline events from last upto the point clicked
+      timeline_events.forEach((ev, idx) => {
+        if (idx >= index) {
+          hideTime(ev);
+        }
+      });
+    } else {
+      timelineProgress(index + 1);
+      // show all timeline events from first upto the point clicked
+      timeline_events.forEach((ev, idx) => {
+        if (idx <= index) {
+          showTime(ev);
+        }
+      });
+    }
+  });
+});
 
-function closeForm2() {
-    document.getElementById("popupForm2").style.display = "none";
-  };
+var doit;
+window.addEventListener("resize", () => {
+  clearTimeout(doit);
+  doit = setTimeout(resizeEnd, 1200);
+});
+
+function resizeEnd() {
+  i = 0;
+  slowLoop();
+}
+
