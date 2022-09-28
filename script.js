dc0907d39f5b399a38c11fc01b08adf73ec01f4d
Osvaldo Felices <osvfelices@yahoo.com>
2022-09-28 12:22:32 +0200

    Adding again the code

--- script.js
+++ script.js
@@ -1,3 +1,48 @@
+// From script
+
+function openForm() {
+
+  document.getElementById("popupForm").style.display = "block";
+  document.getElementById("treenum").value = "";
+  
+  thanksbutton = document.getElementById("thanksbutton");
+  thanksbutton.addEventListener("click", 
+  function thanks(){
+    const name = document.getElementById("name").value;
+    let treenum = document.getElementById("treenum").value;
+
+      switch(true){
+    
+    case (treenum == 1):
+      document.getElementById("message").innerHTML = `${name}, thank you for giving a tree!`;
+      document.getElementById("nametree").value = "";
+      break;
+
+    case (treenum > 1):
+      document.getElementById("message").innerHTML = `${name}, thank you for giving ${treenum} trees!`;
+      document.getElementById("nametree").value = "";
+      break;
+
+    default:
+      document.getElementById("message").innerHTML = `Invalid Number of Trees, please try again`;
+    }
+
+  document.getElementById("popupForm2").style.display = "block";
+  document.getElementById("popupForm").style.display = "none";
+})};
+
+
+function closeForm() {
+  document.getElementById("popupForm").style.display = "none";
+};
+
+
+function closeForm2() {
+  document.getElementById("popupForm2").style.display = "none";
+};
+
+// Timeline Script
+
 const line = document.querySelector(".timeline-innerline");
 
 let i = 0;
